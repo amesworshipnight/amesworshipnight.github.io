a1d5d7aacbe28cf24c6dbdd30163a359e15f1f2e
amesworshipnight <amesworshipteam@gmail.com>
2025-11-03 16:22:35 +0000

    minor edits

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,2 @@
 import { ReactNode } from 'react'
-export default function Card({ children }: { children: ReactNode }) {
-  return (
-    <div className="rounded-2xl bg-white shadow p-4 border border-gray-100">{children}</div>
-  )
-}
+export default function Card({ children }: { children: ReactNode }) { return (<div className="rounded-2xl bg-white shadow p-4 border border-gray-100">{children}</div>) }
